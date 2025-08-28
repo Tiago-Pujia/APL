2a241ea3e48ace16f656f6e10453353b088a1f19
Santiago Manghi Scheck <112188622+Santiago935@users.noreply.github.com>
2025-08-28 16:13:02 -0300

    Archivo cache funcional
    +Ahora contamos con un archivo cache donde se guardan los datos. Alli se consulta primero.
    +Se expande la explicacion del script. Tambien agregue un recordatorio de lo que falta.

--- bash/ejercicio5/ejercicio5.sh
+++ bash/ejercicio5/ejercicio5.sh
@@ -1,18 +1,32 @@
-#Estado: Prototipo acepta varios paises y busca todo. Genera un archivo Json con todos los campos pero no hace nada.
+#Estado: 
+    Prototipo acepta varios paises y busca todo. Genera un archivo Json donde se van guardando los resultados, se elimina al final del proceso.
 
-#1)Ingresar parametros (validarlos)
-#2)Usar los parametros para la API que busque y guarde en archivo cache
-#3)Proceso final (guardar archivo, mostrarlo)
-#4)Limpieza
+#Flujo de datos:
+#1)Ingresar parametros y valida que no tenga numeros ni caracter especial (excepto la ñ)
+#2)Por cada parametro:
+#2.1)Busca el pais en el archivo json. Si no lo encuentra, pide a la API
+#2.2)Extrae los parametros y lo imprime en pantalla
+#3)Al finalizar, borra el archivo cache.
 
+#FALTA:
+#Que acepte por parametro el tiempo de vida en cache
+#Validar los datos con ese tiempo de vida
+#Correguir un error del cual si la API no lo encuentra lanza un mensaje por pantalla.
 
 #Variables
 
-for pais in $@;do
 capital=""
 region=""
 poblacion=0
 moneda=""
+archivo_cache="archivo_cache.json"
+
+if [[ ! -f "$archivo_cache" ]]; then
+    echo "{}" > "$archivo_cache"
+fi
+
+for pais in $@;do
+
 
 #valido
 #que no este vacio
@@ -27,20 +41,33 @@ if ! [[ "$pais" =~ ^[a-zA-ZñÑ\s]+$ ]]; then
     continue
 fi
 
-
-#Guardar en Json la busqueda:
-curl -o test.json https://restcountries.com/v3.1/name/$pais
-
-#consulto API
-resultado=$(curl -s "https://restcountries.com/v3.1/name/$pais")
+#Primero consulto al archivo
+    if jq -e --arg pais "$pais" '.[$pais]' "$archivo_cache" >/dev/null; then
+        echo "Datos desde cache: "
+        resultado=$(jq -c --arg pais "$pais" '.[$pais]' "$archivo_cache")
+    else 
+    #consulto API
+        resultadoAPI=$(curl -s "https://restcountries.com/v3.1/name/$pais")
+            if [[ -z "$resultadoAPI" || "$resultadoAPI" == "[]" ]]; then
+            echo "Error: No se encontró información para '$pais'."
+            continue
+        fi
+        echo "Datos desde API: "
+        #Vuelvo el formato de api al del script
+        resultado=$(echo "$resultadoAPI" | jq '.[0]')  
+        tmp=$(mktemp)
+            jq --arg pais "$pais" --argjson datos "$resultado" \
+           '. + {($pais): $datos}' "$archivo_cache" > "$tmp" && mv "$tmp" "$archivo_cache"
+    
+    fi
 
 #Extraigo campos de interes
-nombre=$(echo "$resultado" | jq -r '.[0].name.common')
-capital=$(echo "$resultado" | jq -r '.[0].capital[0]')
-region=$(echo "$resultado" | jq -r '.[0].region')
-poblacion=$(echo "$resultado" | jq -r '.[0].population')
-moneda_codigo=$(echo "$resultado" | jq -r '.[0].currencies | keys[0]')
-moneda_nombre=$(echo "$resultado" | jq -r ".[0].currencies[\"$moneda_codigo\"].name")
+    nombre=$(echo "$resultado" | jq -r '.name.common')
+    capital=$(echo "$resultado" | jq -r '.capital[0]')
+    region=$(echo "$resultado" | jq -r '.region')
+    poblacion=$(echo "$resultado" | jq -r '.population')
+    moneda_codigo=$(echo "$resultado" | jq -r '.currencies | keys[0]')
+    moneda_nombre=$(echo "$resultado" | jq -r ".currencies[\"$moneda_codigo\"].name")
 
 echo "  Pais: $pais"
 echo "  Capital: $capital"
@@ -49,3 +76,5 @@ echo "  Poblacion: $poblacion"
 echo "  Moneda: $moneda_nombre ($moneda_codigo)"
 
 done
+
+rm -f "$archivo_cache"
