9a80cc013edf14c77f1910ecbc147ddf91978bea
nntomi <107574458+nntomi@users.noreply.github.com>
2025-09-18 15:12:20 -0300

    Ejercicio 3
    Me quedo cortito pero entiendo que funciona bien, cualquier cosa diganme

---
+++ powershell/ejercicio3.ps1
@@ -0,0 +1,22 @@
+
+Param(
+    [Parameter(Mandatory = $True, Position = 1)]
+    [string[]] $Palabras,
+    [Parameter(Mandatory = $True, Position = 2)]
+    [string] $Directorio
+)
+
+Write-Output "Se procede a buscar $Palabras en $Directorio"
+
+Write-Output "-------------------------------------"
+Write-Output "Analizando eventos en logs de sistema"
+Write-Output "-------------------------------------"
+
+$Ruta = Join-Path -Path $Directorio -ChildPath "system.log"
+
+foreach ($item in $Palabras)
+{
+    [int] $cant = (Get-Content $Ruta | Select-String -Pattern $item).count
+        Write-Output("$item : $cant")
+}
+
