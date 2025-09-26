4527dd47429e9177f1c027583ceaa58698f8c8c4
nntomi <107574458+nntomi@users.noreply.github.com>
2025-09-25 22:31:43 -0300

    Arranque pero muy tranqui

---
+++ powershell/ejercicio4.ps1
@@ -0,0 +1,45 @@
+Param(
+    [Parameter(Mandatory = $True, Position = 1)]
+    [String] $repo,
+    [Parameter(Mandatory = $True, Position = 2)]
+    [string] $configuracion,
+    [Parameter(Mandatory = $True, Position = 3)]
+    [string] $log,
+    [Parameter(Mandatory = $False, Position = 4)]
+    [switch] $kill #se activa si se pone, sino queda como false
+)
+
+if($kill){
+    #matar el daemon
+    $pidFile = "/tmp/daemon_$($repo -replace '/', '_').pid"
+    if(Test-Path $pidFile){
+        $pidObtenido = Get-Content $pidFile
+        Stop-Process -Id $pidObtenido -Force
+        Remove-Item $pidFile
+        Write-Output "Daemon detenido (PID: $pidObtenido)"
+    }else{
+        Write-Output "No hay ningun daemone ejecutandose"
+    }
+}
+else{
+    #creo un archivo PID
+    $pidFile = "/tmp/daemon_$($repo -replace '/', '_').pid"
+    $currentPID = $PID
+
+    #verifico si hay uno corriendo
+    if(Test-Path $pidFile){
+        Write-Output "Ya existe un daemon en este repo"
+        exit 1
+    }
+    
+    #Guardo el PID
+    $currentPID | Out-File -FilePath $pidFile
+    Write-Output "Daemon iniciado con PID: $currentPID"
+    Write-Output "Entrando al loop"
+    while ($true) {
+        Write-Output "Probando - PID: $currentPID - $(Get-Date -Format 'HH:mm:ss')"
+        Start-Sleep -Seconds 5
+    }
+}
+
+
